6831fbccd60564431004c93807703bfdbce426bc
Alpha Blog <rivervanblerk@gmail.com>
2019-12-02 15:38:21 +0200

    Add edit, update functionality and partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,11 +42,12 @@ before_action :set_article, only: [:edit, :update, :show, :destroy]
   end
 
   private 
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
